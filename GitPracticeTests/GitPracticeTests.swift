647873723f87ed2558ae6f0671d0c056ab1f20dd
Ipoetzscher <ikpjnk@gmail.com>
2025-04-13 10:17:52 -0700

    Initial Commit

---
+++ GitPracticeTests/GitPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeTests.swift
+//  GitPracticeTests
+//
+//  Created by India Poetzscher on 4/13/25.
+//
+
+import Testing
+@testable import GitPractice
+
+struct GitPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
